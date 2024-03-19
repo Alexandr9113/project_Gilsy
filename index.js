e2fb4b8ab612e9357bfd1aac90236115dd770610
Alexandr9113 <sasha_makaryan@mail.ru>
2024-03-19 15:59:14 +0300

    Create slider in about-salon section

--- index.js
+++ index.js
@@ -19,3 +19,68 @@ function closeMenu() {
   }, 1000);  
 }
 navItems.forEach(el => el.addEventListener('click', closeMenu));
+
+// Slider
+const slider = document.getElementById("slider");
+const slides = document.querySelectorAll(".slider-image");
+const pagination = document.getElementById("pagination");
+
+let currentSlideIndex = 0;
+const paginationCircles = [];
+const sliderWidth = slider.clientWidth;
+
+function createPaginationCircle() {
+    const div = document.createElement("div");
+    div.className = "pagination-circle";
+    pagination.appendChild(div);
+    paginationCircles.push(div);
+}
+
+function addPagination() {
+    slides.forEach(createPaginationCircle);
+    paginationCircles[0].classList.add("active");
+    paginationCircles.forEach((circle, index) => {
+        circle.addEventListener("click", () => changeSlide(index));
+    });
+}
+
+function addActiveClass() {
+    paginationCircles[currentSlideIndex].classList.add("active");
+}
+
+function removeActiveClass() {
+    paginationCircles[currentSlideIndex].classList.remove("active");
+}
+
+function showSlide() {
+    slider.style.transform = `translateX(-${currentSlideIndex * sliderWidth}px)`;
+}
+
+function changeSlide(slideIndex) {
+    removeActiveClass();
+    currentSlideIndex = slideIndex;
+    addActiveClass();
+    showSlide();
+}
+
+function nextSlide() {
+    let newSlideIndex = currentSlideIndex + 1;
+    if(newSlideIndex > slides.length - 1) {
+        newSlideIndex = 0;
+    }
+    changeSlide(newSlideIndex);
+}
+
+function previousSlide() {
+    let newSlideIndex = currentSlideIndex - 1;
+    if(newSlideIndex < 0) {
+        newSlideIndex = slides.length - 1;
+    }
+    changeSlide(newSlideIndex);
+}
+
+setInterval(() => {
+  nextSlide()
+}, 5000);
+
+addPagination();
